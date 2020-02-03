177fdac95d2c090499cfb5186e665bebeeb30aae
aanchal960 <aanchal960@gmail.com>
2020-02-03 12:40:10 +0530

    first commit

---
+++ xyz.bat
@@ -0,0 +1,4 @@
+@echo off 
+python -m pip install --upgrade pip && python -m venv myenv && .\myenv\Scripts\activate.bat && echo "activated venv!"
+py.test tests\test_TrainWatchmanCalculator.py
+echo "Done!!!!!!!"
