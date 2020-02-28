8678fffc3cecf0120861ebdafde18389f4925003
aanchal960 <aanchal960@gmail.com>
2020-02-28 17:51:35 +0530

    test files modified

--- add_sub.bat
+++ add_sub.bat
@@ -1,5 +1,7 @@
 @echo off
 
-python -m pip install --upgrade pip && python -m venv myenv && .\myenv\Scripts\activate.bat && echo "activated venv!" && pip install pytest && py.test tests\test_add.py tests\test_subtract.py --junit-xml=report.xml
+python -m pip install --upgrade pip && python -m venv myenv && .\myenv\Scripts\activate.bat && echo "activated venv!" && pip install pytest && py.test tests\test_add.py tests\test_subtract.py --junit-xml=report.xml 
+
+python reports\xml_to_json.py
 
 echo "Done!!!!!!!"
