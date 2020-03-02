a15154f93f820c779bad82c087936192b8683bbc
aanchal960 <aanchal960@gmail.com>
2020-03-02 17:51:43 +0530

    batch modified

--- add_sub.bat
+++ add_sub.bat
@@ -1,5 +1,5 @@
 @echo off
 
-python -m pip install --upgrade pip && python -m venv myenv && .\myenv\Scripts\activate.bat && echo "activated venv!" && pip install pytest && py.test tests\test_add.py tests\test_subtract.py --junit-xml=reports\report.xml && python reports\xml_to_json.py
+python -m pip install --upgrade pip && python -m venv myenv && .\myenv\Scripts\activate.bat && echo "activated venv!" && pip install pytest && py.test tests\test_add.py tests\test_subtract.py --junit-xml=reports\report.xml && python -m pip install --upgrade pip && pip install xmltodict && python reports\xml_to_json.py
 
 echo "Done!!!!!!!"
