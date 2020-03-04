a540f4af77bc14b4e89be76ebc096b6205075d2c
aanchal960 <aanchal960@gmail.com>
2020-03-04 14:59:36 +0530

    batch modified

--- add_sub.bat
+++ add_sub.bat
@@ -1,6 +1,6 @@
 @echo off
 
-python -m pip install --upgrade pip && python -m venv myenv && .\myenv\Scripts\activate.bat && echo "activated venv!" && pip install pytest && py.test tests\test_add.py tests\test_subtract.py --junit-xml=report.xml && python -m pip install --upgrade pip && pip install xmltodict && python reports\xml_to_json.py && python reports\mongotrial.py && pip install pymongo
+python -m pip install --upgrade pip && python -m venv myenv && .\myenv\Scripts\activate.bat && echo "activated venv!" && pip install pytest && py.test tests\test_add.py tests\test_subtract.py --junit-xml=report.xml && python -m pip install --upgrade pip && pip install xmltodict && python reports\xml_to_json.py && pip install pymongo && python reports\mongotrial.py
 
 
 echo "Done!!!!!!!"
