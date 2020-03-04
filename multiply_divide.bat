a6ccf336e016486c8637c3c2699c2da913caab06
aanchal960 <aanchal960@gmail.com>
2020-03-04 15:07:04 +0530

    batch modified

--- multiply_divide.bat
+++ multiply_divide.bat
@@ -1,6 +1,6 @@
 @echo off
 
-python -m pip install --upgrade pip && python -m venv myenv && .\myenv\Scripts\activate.bat && echo "activated venv!" && pip install pytest && py.test tests\test_multiply.py tests\test_divide.py --junit-xml=report.xml && python -m pip install --upgrade pip && pip install xmltodict && python reports\xml_to_json.py
+python -m pip install --upgrade pip && python -m venv myenv && .\myenv\Scripts\activate.bat && echo "activated venv!" && pip install pytest && py.test tests\test_multiply.py tests\test_divide.py --junit-xml=report.xml && python -m pip install --upgrade pip && pip install xmltodict && python reports\xml_to_json.py&& pip install pymongo && python reports\mongotrial.py
 
 
 echo "Done!!!!!!!"
